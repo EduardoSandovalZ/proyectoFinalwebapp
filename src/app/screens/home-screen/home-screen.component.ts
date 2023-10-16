5d918e66006077a85bc516ec4af4e4159f2c5ecf
EduardoSandovalZ <eduardo.sandovalz@alumno.buap.mx>
2023-10-15 18:44:57 -0600

    Se ve el home

--- src/app/screens/home-screen/home-screen.component.ts
+++ src/app/screens/home-screen/home-screen.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FacadeService } from 'src/app/services/facade.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-home-screen',
@@ -6,14 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-screen.component.scss']
 })
 export class HomeScreenComponent implements OnInit {
-
-  constructor() { }
+  public token : string = "";
+  public lista_usuarios: any[] = [];
+  
+  constructor(
+    private facadeService: FacadeService,
+    private usuariosService: UsuariosService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    //Validar que haya inicio de sesión
+    //Obtengo el token del login
+    this.token = this.facadeService.getSessionToken();
+    console.log("Token: ", this.token);
+    
+    if(this.token == ""){
+      this.router.navigate([""]);
+    }
   }
   //Cerrar sesión
   public logout(){
-    
+    this.facadeService.logout().subscribe(
+      (response)=>{
+        this.facadeService.destroyUser();
+        //Navega al login
+        this.router.navigate(["/"]);
+      }, (error)=>{
+        console.error(error);
+      }
+    );
   }
 
 }
